bd3b0c5ca870dd4faa323afc70fe6fa2f00afe88
FIRAT ATALAY <atafiratbey@gmail.com>
2023-03-16 17:17:55 +0300

    HTML Templating Building And Filling the Templates

--- BEGINNER/SECTION-1/L8-Parsing Variables from URLs/index.js
+++ BEGINNER/SECTION-1/L8-Parsing Variables from URLs/index.js
@@ -34,10 +34,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
@@ -49,7 +49,7 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // Product page
-  } else if (pathName === '/product') {
+  } else if (pathname === '/product') {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
@@ -58,7 +58,7 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json',
     });
